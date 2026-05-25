63631d99464e4f247b5cd53b0773eb6e33b07da2
ChaeyoungYoon <bigtreeycy@naver.com>
2026-05-25 14:52:38 +0900

    week07 윤채영

---
+++ 윤채영/week07/소스코드/MenuRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.cafe.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MenuRequestDto {
+    private String name;
+    private int price;
+    private String category;
+}
